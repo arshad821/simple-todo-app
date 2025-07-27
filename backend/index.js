414c6e710ae8af307eed37353bb65292476b6490
arshad821 <arsharshad977@gmail.com>
2025-07-28 01:50:11 +0530

    url update4

--- backend/index.js
+++ backend/index.js
@@ -52,7 +52,8 @@ app.delete('/api/todos/:id', (req, res) => {
   res.json({ message: "Todo deleted" });
 });
 
-const PORT = process.env.PORT || 5000;
+//const PORT = process.env.PORT || 5000;
+const PORT = 3000;
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
